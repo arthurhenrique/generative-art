ccb8dc928b6f5f8bcd256b329bee3d0fee9f76a7
Arthur Henrique <arthurhenrique@users.noreply.github.com>
2017-01-17 14:32:37 -0200

    Create rosa.py

---
+++ rosa.py
@@ -0,0 +1,23 @@
+import turtle
+ 
+t = turtle.Turtle()
+j = turtle.Screen()
+ 
+def espiral(t, lineLen):
+    if lineLen < 10000:      
+        if(lineLen % 2 == 0):
+            t.pencolor("#FFA500")
+        else:
+            t.pencolor("#00BFFF")          
+        t.forward(lineLen)      
+        t.left(91)            #inclinação 91 graus à esquerda
+        espiral(t,lineLen+3)
+ 
+#inicio
+t.screen.bgcolor("black")
+t.width(1) #ponta caneta
+t.speed(1000)
+espiral(t,0)
+ 
+#saida
+j.exitonclick()
